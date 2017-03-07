56e72a93e796ede1b1e233ac8acf6c0d7d8d52f8
raycheng <ray.cheng.y@gmail.com>
2017-03-07 00:09:43 -0500

    Reordered home, contact, and about pages under pages ctrller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
+
+  root :to => 'pages#index'
+  get "/about" => 'pages#about'
+  get "/contact" => 'pages#contact'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
