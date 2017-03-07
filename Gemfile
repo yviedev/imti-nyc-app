f8fcc6cf7dbf6e346fc854a4c769660aa2040054
Dan <dannguyen90@gmail.com>
2017-03-06 19:21:07 -0500

    add pry to gem file and create strong params for applications controller

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'pry-rails'
   gem 'byebug', platform: :mri
 end
 
--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,30 @@ class ApplicationsController < ApplicationController
   def new
     @application = Application.new
   end
+
+  def create
+    if current_user.update(user_params)
+      @application = Application.new(application_params)
+      if @application.save
+        redirect_to new_application_path
+      else
+        flash[:danger] = @application.errors.full_messages
+        render :new
+      end
+    else
+      flash[:danger] = current_user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :middle_initial)
+  end
+
+  def application_params
+    params.require(:application).permit(:phone_number, :street, :city, :state, :zip_code, :country, :birth_place, :birth_date, :country_of_citizenship, :occupation, :name_of_spouse, :ages_of_children)
+  end
+
 end
