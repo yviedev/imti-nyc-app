2f1f99481c53d57e0bff55d1e295d05cff61e206
Yvonne Ike <yviedev@gmail.com>
2017-03-06 21:00:41 -0500

    Add validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
+  validates :phone_number, :street, :city, :state, :zip_code, :country, :birth_place, :birth_date,           :country_of_citizenship, :occupation, presence: true
   has_many :transcripts, -> { where 'type = transcript' }, class_name: 'Document'
   has_many :recommendations, -> { where 'type = recommendation' }, class_name: 'Document'
   has_many :references, -> { where 'type = reference' }, class_name: 'Document'
