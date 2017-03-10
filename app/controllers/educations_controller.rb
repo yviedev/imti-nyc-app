7e2f09f04a1a0d8810c83cd494c35456a6beb8d8
MattCali <matthewbcalifano@gmail.com>
2017-03-09 20:53:59 -0500

    Make education controller/view/resource routes

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,39 @@
+class EducationsController < ApplicationController
+	def new
+    @application = Application.find(params[:application_id])
+    @education = Education.new
+  end
+
+  def create
+    @education = Education.new(education_params)
+    if @education.save
+      flash[:success] = "Education saved."
+      redirect_to new_application_education_path(@education.id)
+    else
+      flash[:danger] = @education.errors.full_messages
+      render :new
+    end 
+  end
+
+  def edit
+    @education = Education.find(params[:id])
+  end
+
+  def update
+    @education = Education.find(params[:id])
+    if @education.update(education_params)
+      flash[:success] = "education updated."
+      redirect_to edit_education_path(@education.id)
+    else
+      flash[:danger] = @education.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def education_params
+    params.require(:education).permit(:school, :location, :start_date, :end_date, :degree, :degree_date).merge(application_id: params[:application_id]) 
+  end
+end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path: ''
 
-  resources :applications, only: [:new, :create, :edit, :update]
-
   root :to => 'pages#index'
   get "/about" => 'pages#about'
   get "/contact" => 'pages#contact'
   
+  resources :applications, only: [:new, :create, :edit, :update] do
+  	resources :educations, only: [:new, :create, :edit, :update] 
+  end
 end
