af4afc9f0ce6c5c21e1046a2749fff1f7f92843f
Dan <dannguyen90@gmail.com>
2017-03-13 21:00:44 -0400

    complete basic montessori trainings section

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/contact" => 'pages#contact'
 
   resources :applications, only: [:new, :create, :edit, :update] do
-  	resources :educations, only: [:index, :new, :create, :edit, :update] 
+  	resources :educations, only: [:index, :new, :create, :edit, :update]
+  	resources :montessori_trainings, only: [:index, :new, :create, :edit, :update] 
   end
 end
