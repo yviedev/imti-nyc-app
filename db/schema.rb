cab8c0710d29ca51f6d901c9b734845d5918c077
Dan <dannguyen90@gmail.com>
2017-04-20 22:28:35 -0400

    make minor css changes in theme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407004553) do
+ActiveRecord::Schema.define(version: 20170413224218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,8 +73,11 @@ ActiveRecord::Schema.define(version: 20170407004553) do
   create_table "local_schools", force: :cascade do |t|
     t.integer  "code"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "principal_name"
+    t.string   "main_contact"
+    t.string   "address"
   end
 
   create_table "montessori_trainings", force: :cascade do |t|
