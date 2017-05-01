a106c7aa25ab9f394247a5a220e836b681c025ec
Michael Yatco <mjyatco@gmail.com>
2017-05-01 19:10:11 -0400

    Fixed merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170425003011) do
 
   # These are extensions that must be enabled in order to support this database
@@ -79,12 +78,9 @@ ActiveRecord::Schema.define(version: 20170425003011) do
     t.string   "name"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-
-    t.string   "school_logo"
-    t.string   "school_address"
     t.string   "principal_name"
     t.string   "main_contact"
-
+    t.string   "address"
   end
 
   create_table "montessori_trainings", force: :cascade do |t|
