1ee45289bf5774bdfaff35163129e273f3d658fe
John Messina <johnvm914@gmail.com>
2017-05-01 19:12:01 -0400

    fixed csv import error

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-      when ".csv" then Roo::Csv.new(file.path)
+      when ".csv" then Roo::CSV.new(file.path)
       when ".xls" then Roo::Excel.new(file.path)
       when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
